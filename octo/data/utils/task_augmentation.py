253a5891c454592a4cd5ef6f365ed4836b4adb52
marcel ruehle <unesl@student.kit.edu>
2024-06-06 02:55:13 +0200

    i give up...

--- octo/data/utils/task_augmentation.py
+++ octo/data/utils/task_augmentation.py
@@ -3,8 +3,11 @@ Contains basic logic for randomly zero-ing out keys in the task specification.
 """
 
 import pickle
+import csv
+import torch
 
 from huggingface_hub import hf_hub_download
+from torch import Tensor
 import tensorflow as tf
 
 from octo.data.utils.data_utils import to_padding
@@ -48,6 +51,133 @@ class Rephraser:
                 lang_paraphrases = pickle.load(file)
                 # Create StaticHashTable
                 self.rephrase_lookup = self.create_static_hash_table(lang_paraphrases)
+
+
+class LupusRephraser:
+    def create_static_hash_table(self, dictionary):
+        """Takes a python dictionary with string keys and values and creates a tf static hash table"""
+        self.keys = list(dictionary.keys())
+        self.values = tf.ragged.constant(list(dictionary.values()))
+        initializer = tf.lookup.KeyValueTensorInitializer(
+            self.keys, tf.range(len(self.keys))
+        )
+        hash_table = tf.lookup.StaticHashTable(initializer, default_value=-1)
+        return hash_table
+
+    def __init__(self):
+        self.MULTIPLIER = 6364136223846793005
+        self.INCREMENT = 1
+        self.MODULUS = 2**64
+        
+        csv_path = "/home/marcelr/BridgeData/lang_lupus_with_hash.csv"
+        lupus_dict = {}
+        with open(csv_path, newline='') as csv_lupus:
+            reader_lupus = csv.DictReader(csv_lupus, delimiter=';')
+            for row_lupus in reader_lupus:
+                lupus_dict[row_lupus["img_0_md5_hash"]] = []
+                for i in range(15):
+                    lupus_str = row_lupus["language_instruction_" + str(i)]
+                    if lupus_str == "":
+                        break
+                    lupus_dict[row_lupus["img_0_md5_hash"]].append(lupus_str)
+            # Create StaticHashTable
+            self.rephrase_lookup = self.create_static_hash_table(lupus_dict)
+
+    def lookup(self, key):
+        index = self.rephrase_lookup.lookup(key)
+        return tf.cond(tf.reduce_all(tf.equal(index, -1)), lambda: tf.constant(['']), lambda: tf.gather(self.values, index))
+    
+    def hash_tensor(self, x: Tensor) -> Tensor:
+        while x.ndim > 0:
+            x = self._reduce_last_axis(x)
+        return x
+
+    @torch.no_grad()
+    def _reduce_last_axis(self, x: Tensor) -> Tensor:
+        acc = torch.zeros_like(x[..., 0])
+        for i in range(x.shape[-1]):
+            acc *= self.MULTIPLIER
+            acc += self.INCREMENT
+            acc += x[..., i]
+            # acc %= MODULUS  # Not really necessary.
+        return acc
+    
+    # # @tf.function
+    # def hash_tensor(self, x: Tensor) -> Tensor:
+    #     x = tf.strings.to_number(x, out_type=tf.int64)
+    #     # while tf.rank(x) > 0:
+    #     while tf.shape(x).shape[0] > 1:
+    #         x = self._reduce_last_axis(x)
+    #     # return x
+    #     return tf.reduce_sum(x)
+
+    # @tf.function
+    # def _reduce_last_axis(self, x: Tensor) -> Tensor:
+    #     initial_value = tf.zeros(tf.shape(x)[:-1], dtype=tf.int64)
+
+    #     def fold_fn(acc, elem):
+    #         acc = acc * self.MULTIPLIER
+    #         acc = acc + self.INCREMENT
+    #         acc = acc + elem
+    #         return acc
+
+    #     acc = tf.foldr(fold_fn, x, initializer=initial_value)
+    #     return acc
+
+
+def rephrase_lupus(traj: dict):
+    rephraser = LupusRephraser()
+
+    if "language_instruction" not in traj["task"] or "image_primary" not in traj["observation"]:
+        return traj
+    
+    original_language = traj["task"]["language_instruction"]
+    image_hash = rephraser.hash_tensor(traj["observation"]["image_primary"][0])
+
+    dict_is_not_empty = bool(rephraser.rephrase_lookup)
+    if dict_is_not_empty:
+        lupus_instruction = rephraser.lookup(image_hash)
+        num_strings = tf.cast(tf.shape(lupus_instruction)[0], tf.int32)
+        random_index = tf.random.uniform(
+            (tf.shape(original_language)[0],),
+            minval=0,
+            maxval=num_strings,
+            dtype=tf.int32,
+        )
+        sampled_language = tf.gather(lupus_instruction, random_index)
+        traj["task"]["language_instruction"] = sampled_language
+
+    # hash = hashlib.md5(image_0).hexdigest()
+    return traj
+
+
+# def rephrase_lupus(traj: dict):
+#     rephraser = LupusRephraser()
+# 
+#     if "language_instruction" not in traj["task"] or "image_primary" not in traj["observation"]:
+#         return traj
+#     
+#     original_language = traj["task"]["language_instruction"]
+#     # img = traj["observation"]["image_primary"][0]
+#     image_hash = rephraser.hash_tensor(traj["observation"]["image_primary"])
+#     # image_hash = rephraser.hash_tensor(tf.nest.flatten(traj["observation"])[0])
+# 
+#     dict_is_not_empty = bool(rephraser.rephrase_lookup)
+#     if dict_is_not_empty:
+#         # lupus_instruction = rephraser.lookup(tf.strings.as_string(image_hash))
+#         # num_strings = tf.cast(tf.shape(lupus_instruction)[0], tf.int32)
+#         # random_index = tf.random.uniform(
+#         #     (tf.shape(original_language)[0],),
+#         #     minval=0,
+#         #     maxval=num_strings,
+#         #     dtype=tf.int32,
+#         # )
+#         # sampled_language = tf.gather(lupus_instruction, random_index)
+#         # traj["task"]["language_instruction"] = sampled_language
+#         traj["task"]["hash"] = image_hash
+# 
+#     # hash = hashlib.md5(image_0).hexdigest()
+#     return traj
 
 
 def rephrase_instruction(
